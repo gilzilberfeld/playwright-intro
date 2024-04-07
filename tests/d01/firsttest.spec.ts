b3dd656978bcedff9fe28922b8ae27588f0823f3
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-04-07 10:35:46 +0300

    test for d01

---
+++ tests/d01/firsttest.spec.ts
@@ -0,0 +1,22 @@
+import test, { expect } from "@playwright/test";
+
+test('reversal of input', async ({ page }) => {
+    // Navigate to relative URL (from baseurl in config)
+    await page.goto('/d01');
+  
+    // Find the input
+    const inputBox = page.getByRole("textbox", {name : "Input"})
+    
+    // Fill the input
+    await inputBox.fill("abc")
+    
+    // Click the button
+    await page.getByRole("button", {name : 'REVERSE!'}).click()
+
+    // Find the result
+    const resultBox = page.getByRole("textbox", {name : "Result"})
+
+    // Check the result contains the reverse string
+    await expect(resultBox).toHaveValue('cba');
+  });
+  
