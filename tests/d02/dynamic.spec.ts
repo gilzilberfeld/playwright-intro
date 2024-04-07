fb5fe5a22feafb2a9d92b656a723a878769aa0b4
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-04-07 11:37:46 +0300

    d02 test

---
+++ tests/d02/dynamic.spec.ts
@@ -0,0 +1,17 @@
+import test, { expect } from "@playwright/test";
+
+test('error is displayed and hidden', async ({ page }) => {
+    await page.goto('/d02');
+  
+    await page.getByRole("button", {name : 'CHECK'}).click()
+    const errorText = page.getByText("Both values are missing")
+    
+    await expect(errorText).toBeVisible()
+    
+    await page.getByRole("textbox", {name : 'First name'}).fill('a')
+    
+    await expect(errorText).toBeHidden()
+    // OR
+    await expect(errorText).not.toBeVisible()
+})
+  
