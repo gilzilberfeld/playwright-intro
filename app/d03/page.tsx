5c47c608f44c3d0af5ca5cdb97ebf899bb8cc5ac
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-04-07 17:20:29 +0300

    d03 with test

--- app/d03/page.tsx
+++ app/d03/page.tsx
@@ -8,7 +8,7 @@ export default function Demo3() {
   const [theInput, setInput] = useState("");
   const router = useRouter()
 
-  function handleGo(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+  function handleReverse(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
     router.push('/d03/reverse?input=' + theInput)
   }
 
@@ -25,7 +25,7 @@ export default function Demo3() {
             <TextField className="w-1/3 p-2" label="Input" variant="outlined" onChange={handleInputChange}></TextField>
           </div>
           <div className="flex justify-center items-center">
-            <Button className="w-1/2" variant="contained" onClick={handleGo}>
+            <Button className="w-1/2" variant="contained" onClick={handleReverse}>
               Go to reverse page
             </Button>
           </div>
---
+++ tests/demos/d03/navigation.spec.ts
@@ -0,0 +1,23 @@
+import test, { expect } from "@playwright/test";
+
+test('error is displayed and hidden', async ({ page }) => {
+    await page.goto('/d03');
+  
+    const inputBox = page.getByRole("textbox", { name : "Input"})
+    const button =  page.getByRole("button", {name : 'Go to Reverse Page'})
+    
+    await inputBox.fill('navigate')
+    await button.click()
+
+    // after navigation
+    await expect(page).toHaveURL(new RegExp('/.*d03\/reverse'))
+    // OR
+    await expect(page).toHaveURL('d03/reverse?input=navigate')
+
+    const mainHeader = page.getByText('Welcome to the Reverse page!', {exact : true})
+    const message = page.getByText('The reverse of')
+
+    await expect(mainHeader).toBeVisible()
+    await expect(message).toContainText('etagivan')
+})
+  
