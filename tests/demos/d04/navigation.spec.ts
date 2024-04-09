4fae89663dd2ce76e42ece3e0222e65069a546b8
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-04-09 15:54:49 +0300

    d03 + d04

--- tests/demos/d04/navigation.spec.ts
+++ tests/demos/d04/navigation.spec.ts
@@ -1,6 +1,6 @@
 import test, { expect } from "@playwright/test";
 
-test('application navigation', async ({ page }) => {
+test('browser navigation', async ({ page }) => {
     await page.goto('/a03');
   
     const inputBox = page.getByRole("textbox", { name : "Input"})
@@ -9,15 +9,18 @@ test('application navigation', async ({ page }) => {
     await inputBox.fill('navigate')
     await button.click()
 
-    // after navigation
-    await expect(page).toHaveURL(new RegExp('/.*a03\/reverse'))
-    // OR
     await expect(page).toHaveURL('a03/reverse?input=navigate')
-
-    const mainHeader = page.getByText('Welcome to the Reverse page!', {exact : true})
-    await expect(mainHeader).toBeVisible()
     
+    await page.goBack()
+    await expect(page).toHaveURL('/a03')
+
+    await expect(inputBox).toBeEmpty()
+
+    await page.goForward()
+
     const message = page.getByText('The reverse of')
     await expect(message).toContainText('etagivan')
+
+    
 })
   
