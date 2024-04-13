3c27f86ae7ae6a4bad41ef96cff4f2676a9e6922
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-04-13 12:17:24 +0300

    d05

---
+++ tests/demos/d05/browser.context.spec.ts
@@ -0,0 +1,18 @@
+import test, { expect } from "@playwright/test";
+
+test('opening same in same browser window', async ({ context }) => {
+    const firstPage = await context.newPage()
+    await firstPage.goto("/a01")
+    
+    const secondPage = await context.newPage()
+    await secondPage.goto("/a01")
+})
+  
+
+test('opening same page in different browser windows', async ({ browser }) => {
+    const firstPage = await browser.newPage()
+    await firstPage.goto("/a01")
+    
+    const secondPage = await browser.newPage()
+    await secondPage.goto("/a01")
+})
