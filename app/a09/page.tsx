719f1eafb40646f981cd586744527e98f2ff3012
gilzilberfeld <gil.zilberfeld@gmail.com>
2024-04-16 16:59:52 +0300

    demo frames

--- app/a09/page.tsx
+++ app/a09/page.tsx
@@ -12,7 +12,7 @@ export default function App1() {
     <main className="flex min-h-screen flex-col items-center p-10">
       <h1 className="p-2">App 9 - Frames</h1>
       <p className="m-3">In this app, an iframe contains App1 (a01).</p>
-      <iframe src="/a01"  width="400" height="600" title="This here frame" />
+      <iframe src="/a01"  width="400" height="600" title="The Frame" />
     </main>
   );
 }
---
+++ tests/demos/d06/frames.spec.ts
@@ -0,0 +1,11 @@
+import test, { expect } from "@playwright/test";
+
+test("finding the frame", async ({ page }) => {
+  await page.goto("/a09");
+
+  expect(page.mainFrame().childFrames().length).toBe(1);
+  const theFrame = page.mainFrame().childFrames()[0];
+
+  const theResultBox = await theFrame.getByRole("textbox", { name: "Result" });
+  await expect(theResultBox).toHaveValue('Waiting...');
+});
